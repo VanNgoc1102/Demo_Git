5919e67f0d2bf00936f3f04c1d15fd260d81e1e5
VanNgoc1102 <ngocnv.uet@gmail.com>
2022-07-04 17:38:21 +0700

    Demo docker-compose

--- JavaExercise/exception/Child.java
+++
@@ -1,9 +0,0 @@
-package exception;
-//abcbd
-public class Child extends Father {
-    @Override
-    public void showInfo(Object message) throws NullPointerException {
-        super.showInfo(message);
-        // do something
-    }
-}

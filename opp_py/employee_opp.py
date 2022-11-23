f90742cd591def814037ebe4d91eeac880bcb864
Nguyễn Văn Ngọc <ngocnv.uet@gmail.com>
2022-11-23 22:59:49 +0700

    Update employee_opp.py
    Date true or false

--- opp_py/employee_opp.py
+++ opp_py/employee_opp.py
@@ -20,15 +20,28 @@ class Employee:
     def set_raise_amt(cls, amount):
         cls.raise_amt = amount
 
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay =emp_str.split('-')  
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+import datetime
+my_date = datetime.date(2022, 11, 23)
+
+print(Employee.is_workday(my_date))
+
 emp_1 = Employee("Ngoc", "Van", 2002)
 emp_2 = Employee("phong", "an", 2003)
 
 emp_str_1 = 'le-dat-1000'
-emp_str_2 = 'hoang-thai-3000'
-emp_str_3 = 'van-huy-7262'
 
-first, last, pay =emp_str_1.split('-')
-new_emp_1 = Employee(first, last, pay)
+new_emp_1 = Employee.from_string(emp_str_1)
 
 print(new_emp_1.email)
 print(new_emp_1.pay)

d99736f6b8e31e614ef4dd06361d60e77af49009
Nguyễn Văn Ngọc <ngocnv.uet@gmail.com>
2022-11-23 21:18:40 +0700

    Update employee_opp.py

--- opp_py/employee_opp.py
+++ opp_py/employee_opp.py
@@ -1,22 +1,32 @@
 
 class Employee:
     
-    raise_amount = 1.04
+    num_of_emps = 0
+    raise_amt = 1.04
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + '.' + last + '@gmail.com'
+        Employee.num_of_emps += 1
 
     def fullname(self):
         return('{} {}'.format(self.first, self.last))
 
     def apply_raise(self):
-        self.pay = int(self.pay *Employee.raise_amount)
-
+        self.pay = int(self.pay * self.raise_amt)
+    
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
 
 emp_1 = Employee("Ngoc", "Van", 2002)
-print(emp_1.email)
-print(emp_1.fullname())
+emp_2 = Employee("phong", "an", 2003)
 
-print(emp_1.pay)
+Employee.set_raise_amt(1.11)
+
+print(Employee.num_of_emps)
+
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)

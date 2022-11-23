51626f11da0dbcdb9b365bd47f71f992573635bb
Nguyễn Văn Ngọc <ngocnv.uet@gmail.com>
2022-11-23 09:32:16 +0700

    Update robot_opp.py

--- opp_py/robot_opp.py
+++ opp_py/robot_opp.py
@@ -1,17 +1,25 @@
 class  Robot:
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight        
+
     def introduce_self(self):
         print("My name is "+ self.name)
 
-r1 = Robot()
-r1.name = "Tom"
-r1.color = "red"
-r1.weight = 30
+# r1 = Robot()
+# r1.name = "Tom"
+# r1.color = "red"
+# r1.weight = 30
 
 
-r2 = Robot()
-r2.name = "jery"
-r2.color = "blue"
-r2.weight = 18
+# r2 = Robot()
+# r2.name = "jery"
+# r2.color = "blue"
+# r2.weight = 18
+
+r1 = Robot("Tom", "red", 30)
+r2= Robot("jery", "blue", 18)
 
 r1.introduce_self()
 r2.introduce_self()

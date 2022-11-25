a87e4efa1d759305c19ab4335c08cae2729117e2
Nguyễn Văn Ngọc <ngocnv.uet@gmail.com>
2022-11-25 13:36:07 +0700

    Update employee_opp.py

--- opp_py/employee_opp.py
+++ opp_py/employee_opp.py
@@ -38,11 +38,35 @@ class Developer(Employee):
         super().__init__(first, last, pay)
         self.prog_lang = prog_lang
 
+class Manager(Employee):
+    
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('--->', emp.fullname())
 
 
 dev_1 = Developer("Ngoc", "Van", 2000, 'Python')
 dev_2 = Developer("phong", "an", 3000, 'C++')
 
+mgr_1 = Manager("sue", "alex", 900, [dev_1])
+print(mgr_1.email)
 
-print(dev_1.pay)
-print(dev_1.prog_lang)
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
